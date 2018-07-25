356a5d3d238f38306547bbad915d5e22b1ff7465
Donthi <Sridevi.Donthi@unisys.com>
2018-07-25 11:00:56 -0400

    second commit

--- com.learnautomation.cucumber/src/test/java/Runner/TestRunner.java
+++ com.learnautomation.cucumber/src/test/java/Runner/TestRunner.java
@@ -2,11 +2,14 @@ package Runner;
 
 import org.junit.runner.RunWith;
 import cucumber.api.junit.Cucumber;
+import cucumber.api.CucumberOptions;
 
-@RunWith(Cucumber.class)
-@Cucumber.Options(features="features/taggedhooks.feature",
-                  glue={"StepDefinition"},
-                  format={"html:target/Cucumber-html-report"})
+
+//@RunWith(Cucumber.class)
+//@Cucumber.Options(features="features/taggedhooks.feature",
+//                  glue={"StepDefinition"},
+//                  format={"html:target/Cucumber-html-report"})
+
 
 public class TestRunner {
 
